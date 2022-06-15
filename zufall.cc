4637e4650bb740054c76d507dc6493494780cd39
Giuschi <61944612+Giuschi@users.noreply.github.com>
2022-06-15 09:22:07 +0000

    zufall.cc

--- zufall.cc
+++ zufall.cc
@@ -1,2 +1,31 @@
+#include <iostream>
+
+
+double zufall() {
+  int static Ij = 5;
+  int a = 5;
+  int c = 3;
+  int static m = 16;
+  double u =0;
+  Ij = (a*Ij + c) % m;
+  u = ((double) Ij)/m;
+  /* std::cout << Ij << " " << u << std::endl;*/
+  return u;
+}
+
 int main() {
+  int N = 100;
+  int Nin = 0;
+  double x = 0;
+  double y = 0;
+  for(int k = 1; k < N; k++){
+    x = zufall();
+    y = zufall();
+  /*std::cout << x << " " << y << std::endl;*/
+    if(x*x + y*y <= 1){
+      Nin++;
+    }
+    }
+  double out = 4*Nin/N;
+  std::cout << "Nin: " << Nin << "pi: " << out << std::endl;
 }
